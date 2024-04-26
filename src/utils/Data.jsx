4d9eec8add47b341f2bf9b5d418fcb83be5df9a4
Mannthakar24 <210540107514@darshan.ac.in>
2024-04-26 18:34:23 +0530

    added data

--- src/utils/Data.jsx
+++ src/utils/Data.jsx
@@ -75,12 +75,115 @@ export const Data = [
     {
         id: 10,
         title: 'Domestika',
-        description: "Dark Mode Design is a showcase of beautifully designed and inspiring dark mode websites. Dim the lights, lower your screen brightness, and enjoy.",
-        link: 'https://www.darkmodedesign.com/',
-        Category: 'Inspiration',
-        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F1bVTuhsyWy6LfkkArvglcQ%2F20b38e0e3f5813eb47310c60c1235a7d%2Fdarkmodedesign.webp&w=828&q=75'
+        description: "Domestika is the largest creative community. Learn and share from anywhere with online courses taught by the best professionals in the world of design and creativity.",
+        link: 'https://www.domestika.org/',
+        Category: 'IUpskilling',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F70u1tcTs2v9gGKiD0HXrjg%2Fb8498eea8c9080bfde57a061bbdf2774%2Fdomestika.webp&w=828&q=75'
     },
-
+    {
+        id: 11,
+        title: 'Adobe Fonts',
+        description: "Adobe Fonts partners with the world’s leading type foundries to bring thousands of beautiful fonts to designers every day. No need to worry about licensing, and you can use fonts from Adobe Fonts on the web or in desktop applications.",
+        link: 'https://fonts.adobe.com/',
+        Category: 'Typography',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F7wVYCRyn17GY8uzt8AYQSi%2F7bd890c2f46b6c0c287e5ebae8a53783%2Fadobefonts.webp&w=828&q=75'
+    },
+    {
+        id: 12,
+        title: 'Arnau Ros',
+        description: "Videos about Web Design, Webflow, Freelancing, and No Code.",
+        link: 'https://www.youtube.com/c/arnauros',
+        Category: 'YT Channels',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F7mtY4vM3kIOFW7JkS1Uj4W%2Fe53a14ea65a1a3a01dda1e59694aa60c%2Farnau.webp&w=828&q=75'
+    },
+    {
+        id: 13,
+        title: 'Art Directed Mockups',
+        description: "Thoughtfully crafted, realistic, premium mockups that do not look like everything else. Improve your branding presentations, social media presence and delight your clients. All mockups have real shadows, beautiful art directed scenes and urban scenarios and are over 5000px. Art Directed is a design resources company.",
+        link: 'https://artdirected.design/',
+        Category: 'Tools',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2FnYOPhaIYh9IXCSgmQzB37%2Fa70c5b160a4cc390546abcf3f638e607%2Fartdirected.webp&w=828&q=75'
+    },
+    {
+        id: 14,
+        title: 'CleanShot X',
+        description: "CleanShot X is the best screenshot and screen recording app for Mac with a built-in annotation tool, Cloud uploading, scrolling capture, and a lot more.",
+        link: 'https://cleanshot.com/',
+        Category: 'Tools',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F4YvvGDwvP5HkkxUQZmmJpI%2Fceb2093941e144f12c8b7f42c60888c3%2FScreen_Studio-2.webp&w=828&q=75'
+    },
+    {
+        id: 15,
+        title: 'Codrops',
+        description: "Keeping web professionals inspired & up-to-date since 2009",
+        link: 'https://tympanus.net/codrops/',
+        Category: 'Inspiration',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F3o3qrvdUtSGY4H2eUfUeyf%2F50225fc64047073413ef2004178568b2%2Fcodrops.webp&w=828&q=75'
+    },
+    {
+        id: 16,
+        title: 'Coursera',
+        description: "Learn new job skills in online courses from industry leaders like Google, IBM, & Meta. Advance your career with top degrees from Michigan, Penn, Imperial & more.",
+        link: 'https://www.coursera.org/',
+        Category: 'Upskilling',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F2GNuB5UtQGWwGyVthR5M0t%2F19414a828801de6446d227b7c5d64abe%2Fcoursera.webp&w=828&q=75'
+    },
+    {
+        id: 17,
+        title: 'Doing Cool Stuff',
+        description: "A directory of brand, motion and digital product design studios doing cool stuff.",
+        link: 'https://www.doingcoolstuff.xyz/',
+        Category: 'Inspiration',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F1LIipBuT0yfY9qjPa7SYuE%2Fce5ba28efeb92075fd7920cd9606983d%2Fdoingcoolstuff.webp&w=828&q=75'
+    },
+    {
+        id: 18,
+        title: 'Easings',
+        description: "This page helps you choose the right easing function",
+        link: 'https://easings.net/',
+        Category: 'Tools',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F3vgFTemHLtrFBA26sTcmA6%2F34586cbfd94be062192cd94f7065267a%2Feasings.webp&w=828&q=75'
+    },
+    {
+        id: 19,
+        title: 'Figma',
+        description: "Figma is the leading collaborative design tool for building meaningful products. Seamlessly design, prototype, develop, and collect feedback in a single platform.",
+        link: '',
+        Category: 'Tools',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F44JdwbgyJBqUUSrEwbvQvP%2Fefa13adf0a12939881b2ea452235e12f%2FFigma__2_.webp&w=828&q=75'
+    },
+    {
+        id: 20,
+        title: 'Fireship',
+        description: "High-intensity ⚡ code tutorials and tech news to help you ship your app faster. New videos every week covering the topics every programmer should know.",
+        link: 'https://www.youtube.com/c/fireship/featured',
+        Category: 'YT Channels',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F2GMD5zcoVuOBjHHZhRY3aV%2Fd8590ca192154d6763086b1d3d1c3676%2Ffireship.webp&w=828&q=75'
+    },
+    {
+        id: 21,
+        title: 'Flux Academy',
+        description: "Learn to be a more successful and profitable web designer: master the latest trends, tools, and technologies, and make it easy to get and retain high-value dream clients. Overall, a great channel for web design and freelancing.",
+        link: 'https://www.youtube.com/@FluxAcademy',
+        Category: 'YT Channels',
+        img: 'https://www.pillarstack.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Ffwy0yv14lkat%2F3lteRUtHIwxJtP3yNSm5Xp%2F3372f34104bedbd25070106cfeb3f827%2Ffluxacademy.webp&w=828&q=75'
+    },
+    {
+        id: 12,
+        title: '',
+        description: "",
+        link: '',
+        Category: '',
+        img: ''
+    },
+    {
+        id: 12,
+        title: '',
+        description: "",
+        link: '',
+        Category: '',
+        img: ''
+    },
 
 
 ]
